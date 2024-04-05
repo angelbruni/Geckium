c0b1284b26960e7907e78cf703c556f5d9a1369e
angelbruni <48385307+angelbruni@users.noreply.github.com>
2024-04-05 01:06:43 +0100

    GeckiumMaterial - navigation drawer.
    - Fix "mini" and rename it to "rail";
    - Prepare welcome page that will be used to test the "jump to page" implementation.

---
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/modal.js
@@ -0,0 +1,32 @@
+
+
+
+
+modalToggles.forEach(modalToggle => {
+  modalToggle.addEventListener('click', function() {
+    
+    
+
+    if (modalTarget.classList.contains('active')) {
+      
+    } else {
+      
+    }
+  });
+});
+
+modals.forEach(modal => {
+    modal.addEventListener('click', function(event) {
+        if (!event.target.closest('.card')) {
+            if (modal.classList.contains('active')) {
+              
+            }
+        }
+    });
+});
+
+window.addEventListener('load', function() {
+  modals.forEach(modal => {
+    
+  });
+});
