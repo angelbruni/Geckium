ba3e86f2037ea7223bad0ab12a57a45d580a4325
angelbruni <48385307+angelbruni@users.noreply.github.com>
2024-04-05 19:28:30 +0100

    Attempt to fix macOS stuff breaking Windows stuff

--- Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
@@ -3,3 +3,27 @@ function windowTitle() {
 }
 
 document.addEventListener("DOMContentLoaded", windowTitle);
+
+
+
+window.addEventListener('load', function() {
+	restoreButtons.forEach(restoreButton => {
+
+	})
+});
+maximizeButtons.forEach(maximizeButton => {
+    maximizeButton.addEventListener("click", () => {
+        
+		restoreButtons.forEach(restoreButton => {
+			
+		})
+    });
+});
+restoreButtons.forEach(restoreButton => {
+    restoreButton.addEventListener("click", () => {
+        
+		maximizeButtons.forEach(maximizeButton => {
+			
+		})
+    });
+});
