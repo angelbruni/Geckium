ef900443bf306db973f21d46df47946d10fcb6dd
angelbruni <48385307+angelbruni@users.noreply.github.com>
2024-04-19 01:02:04 +0100

    Rename stuff
    - Geckium Settings/settings/gsettings to Geckium Flags/gflags;
    - crFlags to chrFlags.

--- Profile Folder/chrome/JS/Geckium_toolbarButtonCreator.uc.js
+++ Profile Folder/chrome/JS/Geckium_toolbarButtonCreator.uc.js
@@ -510,7 +510,7 @@ const menu_page = {
 }
 
 window.addEventListener("load", function() {
-	createToolbarbutton("gsettings", "", "Geckium Settings", "", true, false, CustomizableUI.AREA_NAVBAR, openSettings)
+	createToolbarbutton("gflags", "", "Geckium Flags", "", true, false, CustomizableUI.AREA_NAVBAR, openGFlags)
 	geckiumCreateMenu("page", "Page Menu", "Control the current page", menu_page);
 
 
