24dcb62d48e0660984a01222d9a677db3384bdc4
angelbruni <48385307+angelbruni@users.noreply.github.com>
2024-04-24 20:08:04 +0100

    GeckiumMaterial - open and close window animation

--- Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
@@ -3,26 +3,47 @@ function windowTitle() {
 }
 document.addEventListener("DOMContentLoaded", windowTitle);
 
+function windowClose() {
+	document.getElementById("window").classList.add("closing");
 
+	setTimeout(() => {
+		window.close();
+	}, 300);
+}
+
+let closeButton = document.querySelector(".caption-button.close");
+let maximizeButton = document.querySelector(".caption-button.maximize");
+let restoreButton = document.querySelector(".caption-button.restore");
+let minimizeButton = document.querySelector(".caption-button.minimize");
 
 window.addEventListener('load', function() {
-	restoreButtons.forEach(restoreButton => {
+	if (restoreButton)
+		restoreButton.style.display = "none";
+});
 
-	})
-});
-maximizeButtons.forEach(maximizeButton => {
-    maximizeButton.addEventListener("click", () => {
-        
-		restoreButtons.forEach(restoreButton => {
-			
-		})
-    });
-});
-restoreButtons.forEach(restoreButton => {
-    restoreButton.addEventListener("click", () => {
-        
-		maximizeButtons.forEach(maximizeButton => {
-			
-		})
-    });
-});
+if (closeButton) {
+	closeButton.addEventListener("click", () => {
+		windowClose();
+	});
+}
+
+if (maximizeButton) {
+	maximizeButton.addEventListener("click", () => {
+		maximizeButton.style.display = "none";
+	
+		restoreButton.style.display = "flex";
+	});
+}
+if (restoreButton) {
+	restoreButton.addEventListener("click", () => {
+		restoreButton.style.display = "none";
+	
+		maximizeButton.style.display = "flex";
+	});
+}
+
+if (minimizeButton) {
+	minimizeButton.addEventListener("click", () => {
+		window.minimize();
+	});
+}
