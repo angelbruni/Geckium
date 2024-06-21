1372ce5ddfcb9490826ad055a35560a26d5fbf41
dominichayesferen <ferenosdev@outlook.com>
2024-06-21 01:06:54 +0100

    More gradient accuracy, and reference accurate Incognito palettes

--- Profile Folder/chrome/JS/Geckium_appearance.uc.js
+++ Profile Folder/chrome/JS/Geckium_appearance.uc.js
@@ -400,26 +400,19 @@ class gkGTK {
 			`rgb(${ColorUtils.HSLShift(rgb, [-1, 0.5, 0.75])})`
 		);
 		//Incognito (active)
-		rgb = ColorUtils.HSLShift(rgb, [-1, 0.2, 0.35]);
+		var rgbb = ColorUtils.HSLShift(rgb, [-1, 0.2, 0.35]);
 		document.documentElement.style.setProperty(
 			`--incognito-active`,
-			`rgb(${rgb})`
+			`rgb(${rgbb})`
 		);
 		document.documentElement.style.setProperty(
 			`--incognito-active-shine`,
-			`rgb(${ColorUtils.HSLShift(rgb, [-1, -1, 0.58])})`
+			`rgb(${ColorUtils.HSLShift(rgbb, [-1, -1, 0.58])})`
 		);
 		//Background Tab background (Incognito)
 		document.documentElement.style.setProperty(
 			`--incognito-bgtab-background`,
-			`rgb(${ColorUtils.HSLShift(rgb, [-1, 0.5, 0.75])})`
-		);
-		//InactiveCaption
-		colorDiv.style.backgroundColor="InactiveCaption";
-		rgb = window.getComputedStyle(colorDiv)["background-color"].match(/\d+/g);
-		document.documentElement.style.setProperty(
-			`--inactivecaption-shine`,
-			`rgb(${ColorUtils.HSLShift(rgb, [-1, -1, 0.58])})`
+			`rgb(${ColorUtils.HSLShift(rgbb, [-1, 0.5, 0.75])})`
 		);
 		//Incognito (inactive)
 		rgb = ColorUtils.HSLShift(rgb, [-1, 0.3, 0.6]);
@@ -431,11 +424,18 @@ class gkGTK {
 			`--incognito-inactive-shine`,
 			`rgb(${ColorUtils.HSLShift(rgb, [-1, -1, 0.58])})`
 		);
+		//InactiveCaption
+		colorDiv.style.backgroundColor="InactiveCaption";
+		rgb = window.getComputedStyle(colorDiv)["background-color"].match(/\d+/g);
+		document.documentElement.style.setProperty(
+			`--inactivecaption-shine`,
+			`rgb(${ColorUtils.HSLShift(rgb, [-1, -1, 0.58])})`
+		);
 		//Pre-6.0 toolbar icon fill colour
 		colorDiv.style.backgroundColor="AccentColor";
 		rgb = window.getComputedStyle(colorDiv)["background-color"].match(/\d+/g);
 		colorDiv.style.backgroundColor="-moz-dialog";
-		var rgbb = window.getComputedStyle(colorDiv)["background-color"].match(/\d+/g);
+		rgbb = window.getComputedStyle(colorDiv)["background-color"].match(/\d+/g);
 		if (Math.abs(ColorUtils.ColorToHSL(rgb)[2] - ColorUtils.ColorToHSL(rgbb)[2]) < 0.1) {
 			// Not enough contrast - use foreground
 			document.documentElement.style.setProperty(
