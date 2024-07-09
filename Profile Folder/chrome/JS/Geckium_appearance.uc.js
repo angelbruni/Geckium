536c9f4c35863461079f67c51b0794dff4d6b1a2
dominichayesferen <ferenosdev@outlook.com>
2024-07-09 01:29:12 +0100

    New handling of Firefox Themes

--- Profile Folder/chrome/JS/Geckium_appearance.uc.js
+++ Profile Folder/chrome/JS/Geckium_appearance.uc.js
@@ -8,70 +8,6 @@
 // @include		about:addons*
 // ==/UserScript==
 
-class gkLWTheme {
-	static get getCustomThemeMode() {
-		const customThemeModePref = gkPrefUtils.tryGet("Geckium.customtheme.mode").int;
-		let customThemeMode;
-
-		if (customThemeModePref <= 0)
-			customThemeMode = 0;
-		else if (customThemeModePref == 1)
-			customThemeMode = 1;
-		else if (customThemeModePref >= 2)
-			customThemeMode = 2;
-
-		return customThemeMode;
-	}
-
-	static setCustomThemeModeAttrs() {
-		if (typeof document.documentElement !== "undefined") {	
-			setTimeout(async () => {
-				document.documentElement.setAttribute("lwtheme-id", gkPrefUtils.tryGet("extensions.activeThemeID").string);
-				document.documentElement.setAttribute("customthememode", gkLWTheme.getCustomThemeMode);
-
-				const toolbarBgColor = getComputedStyle(document.documentElement).getPropertyValue('--toolbar-bgcolor'); 
-
-				// if color is rgba
-				if (toolbarBgColor.includes("rgba")) {
-					document.documentElement.setAttribute("toolbar-bgcolor-transparent", true);
-
-					const toolbarBgColorClean = toolbarBgColor.replace("rgba(", "").replace(")", "");
-					const toolbarBgColorToArray = toolbarBgColorClean.replace(" ", "").split(",");
-
-					// if alpha is not opaque
-					if (toolbarBgColorToArray[3] == 0 || toolbarBgColorToArray[3].includes(".")) {
-						document.documentElement.style.setProperty("--gktoolbar-bgcolor", `rgb(${toolbarBgColorToArray[0]}, ${toolbarBgColorToArray[1]}, ${toolbarBgColorToArray[2]})`);
-					} else {
-						document.documentElement.style.setProperty("--toolbar-bgcolor", `rgb(${toolbarBgColorToArray[0]}, ${toolbarBgColorToArray[1]}, ${toolbarBgColorToArray[2]})`);
-					}
-				} else {
-					document.documentElement.setAttribute("toolbar-bgcolor-transparent", false);
-
-					document.documentElement.style.removeProperty("--gktoolbar-bgcolor");
-				}
-				
-				// await gkChromiumFrame.automatic();
-			}, 0);
-		}
-	}
-}
-
-window.addEventListener("load", gkLWTheme.setCustomThemeModeAttrs);
-Services.obs.addObserver(gkLWTheme.setCustomThemeModeAttrs, "lightweight-theme-styling-update");
-
-
-/* bruni: Automatically apply appearance and theme
-		  attributes when it detecs changes in the pref. */
-const appearanceObserverOld = {
-	observe: function (subject, topic, data) {
-		if (topic == "nsPref:changed") {
-			gkLWTheme.setCustomThemeModeAttrs();
-		}
-	},
-};
-Services.prefs.addObserver("Geckium.main.overrideStyle", appearanceObserverOld, false);
-Services.prefs.addObserver("Geckium.main.style", appearanceObserverOld, false);
-
 function changePrivateBadgePos() {
 	if (typeof document.documentElement !== "undefined") {
 		if (document.documentElement.hasAttribute("privatebrowsingmode")) {
@@ -90,12 +26,11 @@ function customThemeColorizeTabGlare() {
 const customThemeModeObserver = {
 	observe: function (subject, topic, data) {
 		if (topic == "nsPref:changed") {
-			gkLWTheme.setCustomThemeModeAttrs();
+			// gkLWTheme.setCustomThemeModeAttrs();
 			customThemeColorizeTabGlare();
 		}
 	},
 };
-window.addEventListener("load", gkLWTheme.setCustomThemeModeAttrs);
 window.addEventListener("load", customThemeColorizeTabGlare);
 Services.prefs.addObserver("Geckium.customtheme.mode", customThemeModeObserver, false);
 Services.prefs.addObserver("Geckium.appearance.customThemeColorizeTabGlare", customThemeModeObserver, false);
