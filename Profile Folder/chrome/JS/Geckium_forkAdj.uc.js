a37cd8b8cf7c5a3aa3c98eb3ee86703a5f877547
dominichayesferen <ferenosdev@outlook.com>
2024-08-09 03:21:01 +0100

    Added... support for THAT...

--- Profile Folder/chrome/JS/Geckium_forkAdj.uc.js
+++ Profile Folder/chrome/JS/Geckium_forkAdj.uc.js
@@ -100,5 +100,53 @@ if (AppConstants.MOZ_APP_NAME == "waterfox") {
             }
         },
     };
-    Services.prefs.addObserver("browser.theme.enableWaterfoxCustomizations", sysThemeObserver, false);
+    Services.prefs.addObserver("browser.theme.enableWaterfoxCustomizations", waterfoxObserver, false);
+}
+
+// r3dfox Adjustments
+//  Yes, we know... you don't need to tell us about it - we're only doing this out of neutrality.
+class gkRfoxAdj {
+    static blacklist = [
+        "r3dfox.caption.text.color",
+        "r3dfox.colors.enabled",
+        "r3dfox.customizations.enabled",
+        "r3dfox.force.transparency",
+        "r3dfox.transparent.menubar",
+        "r3dfox.translucent.navbar"
+    ]
+
+    /**
+     * disableThemeCusto - Ensures R3dfox's theme customisation options are turned off
+     */
+
+    static disableThemeCusto() {
+        var changes = 0;
+        for (const i in gkRfoxAdj.blacklist) {
+            if (gkPrefUtils.tryGet(gkRfoxAdj.blacklist[i]).bool != false) {
+                gkPrefUtils.set(gkRfoxAdj.blacklist[i]).bool(false);
+                changes += 1;
+            }
+        }
+        if (changes >= 1) {
+            _ucUtils.showNotification({
+                label : "r3dfox theme customisations are not supported by Geckium and have been disabled.",
+                type : "geckium-notification",
+                priority: "critical"
+            })
+        }
+	}
+}
+if (AppConstants.MOZ_APP_NAME == "r3dfox") {
+    window.addEventListener("load", gkRfoxAdj.disableThemeCusto);
+    // Automatically change the titlebar when the setting changes
+    const rfoxObserver = {
+        observe: function (subject, topic, data) {
+            if (topic == "nsPref:changed") {
+                gkRfoxAdj.disableThemeCusto();
+            }
+        },
+    };
+    for (const i in gkRfoxAdj.blacklist) {
+        Services.prefs.addObserver(gkRfoxAdj.blacklist[i], rfoxObserver, false);
+    }
 }
