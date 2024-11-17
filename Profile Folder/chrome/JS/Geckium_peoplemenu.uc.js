16514fc78ec435cb2572aecde0577f3e8dc3dd1e
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2024-11-17 14:07:06 +0100

    Replace waitForElement with waitForElm

--- Profile Folder/chrome/JS/Geckium_peoplemenu.uc.js
+++ Profile Folder/chrome/JS/Geckium_peoplemenu.uc.js
@@ -7,42 +7,24 @@
 (function() {
     'use strict';
 
-    // Retry function to ensure elements exist before trying to add event listeners
-    function waitForElement(selector, callback, maxRetries = 20, delay = 500) {
-        let retries = 0;
+	function modifyButtons() {
+		// Firefox Monitor
+		waitForElm('#PanelUI-fxa-menu #PanelUI-fxa-menu-monitor-button').then((monitorButton) => {
+			monitorButton.addEventListener('click', function(event) {
+				event.preventDefault();
+				openTrustedLinkIn("about:profiles", "tab");
+			});
+		});
 
-        const interval = setInterval(function() {
-            const element = document.querySelector(selector);
-            if (element || retries >= maxRetries) {
-                clearInterval(interval);
-                if (element) {
-                    callback(element);
-                } else {
-                }
-            }
-            retries++;
-        }, delay);
-    }
+		// Firefox R(D)elay
+		waitForElm('#PanelUI-fxa-menu #PanelUI-fxa-menu-relay-button').then((relayButton) => {
+			relayButton.addEventListener('click', function(event) {
+				event.preventDefault();
+				OpenBrowserWindow({ private: true });
+			});
+		});
+	}
 
-    function modifyButtons() {
-        // Firefox Monitor
-        waitForElement('#PanelUI-fxa-menu #PanelUI-fxa-menu-monitor-button', function(monitorButton) {
-
-                monitorButton.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    openTrustedLinkIn("about:profiles", "tab")
-                });
-        });
-
-        // Firefox R(D)elay
-        waitForElement('#PanelUI-fxa-menu #PanelUI-fxa-menu-relay-button', function(relayButton) {
-
-                relayButton.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    OpenBrowserWindow({ private: true });
-                });
-        });
-    }
 
     window.addEventListener('load', function() {
         modifyButtons();
