16bd9174ff5f1ece612d33553de7622d7ed9d7c7
angelbruni <48385307+angelbruni@users.noreply.github.com>
2025-02-06 19:43:47 +0000

    I thought I changed this before pushing yesterday

--- Profile Folder/chrome/JS/Geckium_toolbar.uc.js
+++ Profile Folder/chrome/JS/Geckium_toolbar.uc.js
@@ -6,7 +6,7 @@
 
 UC_API.Runtime.startupFinished().then(() => {
 	// Maybe temporary fix to the forced vertical spacer...?? Why did you do this Mozilla...
-	let forcedSpacer = document.querySelector(`#vertical-spacer[removable="false"]`);
+	let forcedSpacer = document.querySelector(`#vertical-spacer`);
 	if (forcedSpacer) {
 		forcedSpacer.setAttribute("removable", true);
 		CustomizableUI.removeWidgetFromArea("vertical-spacer");
