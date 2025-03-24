1e35134605183733846e610a198f6ce547028d91
dominichayesferen <ferenosdev@outlook.com>
2025-03-24 19:25:29 +0000

    Old Geck yells at user, didn't compile Geckium

--- Profile Folder/chrome/JS/Geckium_wizard.uc.js
+++ Profile Folder/chrome/JS/Geckium_wizard.uc.js
@@ -5,11 +5,24 @@
 // ==/UserScript==
 
 window.addEventListener("load", () => {
+	// Ensure Geckium is actually installed
+	let geckCheck = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+	geckCheck.initWithPath(`${FileUtils.getDir("ProfD", []).path.replace(/\\/g, "/")}/chrome/userChrome.css`);
+	if (!geckCheck.exists()) {
+		UC_API.Notifications.show({
+			label : "Welcome to ium! To get the full Geckium experience, download Geckium from Releases or compile your chrome folder.",
+			type : "geckium-notification",
+			priority: "critical"
+		});
+		return;
+	}
+	
 	// Migrate Silverfox settings before the wizard can be shown
 	if (!gkPrefUtils.tryGet("Geckium.firstRun.complete").bool && !gkPrefUtils.tryGet("Geckium.firstRun.wasSilverfox").bool && sfMigrator.getWasSf)
 		sfMigrator.migrate();
 
-	// Show Geckium splash screen
-	if (!gkPrefUtils.tryGet("Geckium.firstRun.complete").bool)
+	// Show Geckium splash screen if Geckium is installed properly
+	if (!gkPrefUtils.tryGet("Geckium.firstRun.complete").bool) {
 		openGSplash();
+	}
 });
